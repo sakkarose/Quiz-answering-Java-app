56f91ef87bc8ffc2314bfcba97940a411f9d67d2
sakkarose <45507664+sakkarose@users.noreply.github.com>
2021-12-18 21:41:55 +0700

    Update AddQuestionActivity.java

--- app/src/main/java/sakkarose/androidgame/AiLaTrieuPhu/AddQuestionActivity.java
+++ app/src/main/java/sakkarose/androidgame/AiLaTrieuPhu/AddQuestionActivity.java
@@ -3,20 +3,29 @@ package sakkarose.androidgame.AiLaTrieuPhu;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class AddQuestionActivity extends AppCompatActivity
 {
 
-
+    ImageView ivback;
+    TextView tvquestion, tvanswerA, tvanswerB, tvanswerC, tvanswerD;
     MediaPlayer mp;
 
     private void findViewsByIds()
     {
+        tvquestion = (TextView) findViewById(R.id.tv_question);
+        tvanswerA = (TextView) findViewById(R.id.tv_answer1);
+        tvanswerB = (TextView) findViewById(R.id.tv_answer2);
+        tvanswerC = (TextView) findViewById(R.id.tv_answer3);
+        tvanswerD = (TextView) findViewById(R.id.tv_answer4);
 
-
-
+        ivback = (ImageView) findViewById(R.id.iv_back);
     }
 
     @Override
